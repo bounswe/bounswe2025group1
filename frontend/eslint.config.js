04bf18256d5f81bc69fa6988177af20bc8141619
Süleyman Tolga Acar <tolgasuleymanacar@gmail.com>
2025-04-15 19:07:41 +0300

    Fix eslint configuration conflict

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import reactPlugin from 'eslint-plugin-react'
 
 export default [
   { ignores: ['dist'] },
@@ -19,15 +20,20 @@ export default [
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'react': reactPlugin,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      // From Airbnb config, we're adding specific rules that were included
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      // Added from .eslintrc.js
+      'react/react-in-jsx-scope': 'off', // Not needed with Vite + React 17+
+      'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.js'] }],
     },
   },
 ]
