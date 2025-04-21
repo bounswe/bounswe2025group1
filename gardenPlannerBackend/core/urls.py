07ebee2e6b1b79aed9e5e37b3bd9cea58e599765
Bahadır Kuşcan <149206458+bahadirkuscan@users.noreply.github.com>
2025-04-21 17:16:49 +0300

    Added URLs for Register, Login, Logout

--- gardenPlannerBackend/core/urls.py
+++ gardenPlannerBackend/core/urls.py
@@ -24,6 +24,9 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     # Add api/ path when you create app-specific URLs
     # path('api/', include('garden.urls')),
+    path('api/register/', RegisterView.as_view(), name='register'),
+    path('api/login/', CustomLoginView.as_view(), name='login'),
+    path('api/logout/', LogoutView.as_view(), name='logout')
 ]
 
 # Serve media files in development
