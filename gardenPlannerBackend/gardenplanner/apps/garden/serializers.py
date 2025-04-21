42f06c66dcaab30f30373af1e77174f207c7d0d1
Bahadır Kuşcan <149206458+bahadirkuscan@users.noreply.github.com>
2025-04-21 17:26:29 +0300

    Added Register Serializer
    Profile model is needed

--- gardenPlannerBackend/gardenplanner/apps/garden/serializers.py
+++ gardenPlannerBackend/gardenplanner/apps/garden/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 # Serializers will be implemented later
 # For example:
@@ -8,3 +9,26 @@ from rest_framework import serializers
 # 
 # class GardenSerializer(serializers.ModelSerializer):
 #     pass 
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    location = serializers.CharField(required=False, allow_blank=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password', 'location']
+
+    def create(self, validated_data):
+        location = validated_data.pop('location', None)  # handle optional
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            password=validated_data['password'],
+        )
+        # PROFILE MODEL IS NEEDED
+        if location:
+            user.profile.location = location  # assuming a profile model exists
+            user.profile.save()
+        return user
