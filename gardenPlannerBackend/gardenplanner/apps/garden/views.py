a9b890e20e197d99760254e43787ce1489b4a7b7
cqberk <ceylanberk.tola@std.bogazici.edu.tr>
2025-04-22 14:12:36 +0300

    profile endpoints completed

--- gardenPlannerBackend/gardenplanner/apps/garden/views.py
+++ gardenPlannerBackend/gardenplanner/apps/garden/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
 from rest_framework.views import APIView
@@ -7,7 +7,8 @@ from rest_framework import viewsets, status, permissions
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.serializers import AuthTokenSerializer
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, ProfileSerializer, UserSerializer, ProfileUpdateSerializer, FollowSerializer
+from .models import Profile
 from django.contrib.auth.forms import PasswordResetForm
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
@@ -15,7 +16,8 @@ from django.contrib.auth.tokens import default_token_generator
 from django.template.loader import render_to_string
 from django.conf import settings
 from django.core.mail import send_mail
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators import action
 
 
 # Views will be implemented later
@@ -54,6 +56,76 @@ class LogoutView(APIView):
         request.user.auth_token.delete()
         logout(request)
         return Response({'success': 'Logged out successfully'}, status=status.HTTP_200_OK)
+
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        """Get current user's profile"""
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+    
+    def put(self, request):
+        """Update current user's profile"""
+        serializer = ProfileUpdateSerializer(request.user.profile, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(ProfileSerializer(request.user.profile).data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, user_id):
+        """Get another user's profile"""
+        user = get_object_or_404(User, id=user_id)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+
+class FollowView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def post(self, request):
+        """Follow a user"""
+        serializer = FollowSerializer(data=request.data)
+        if serializer.is_valid():
+            user_to_follow = get_object_or_404(User, id=serializer.validated_data['user_id'])
+            request.user.profile.follow(user_to_follow.profile)
+            return Response({'status': 'following'}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request):
+        """Unfollow a user"""
+        serializer = FollowSerializer(data=request.data)
+        if serializer.is_valid():
+            user_to_unfollow = get_object_or_404(User, id=serializer.validated_data['user_id'])
+            request.user.profile.unfollow(user_to_unfollow.profile)
+            return Response({'status': 'unfollowed'}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class FollowersListView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        """Get list of users following the current user"""
+        followers = request.user.profile.followers.all()
+        serializer = ProfileSerializer(followers, many=True)
+        return Response(serializer.data)
+
+
+class FollowingListView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        """Get list of users that the current user is following"""
+        following = request.user.profile.following.all()
+        serializer = ProfileSerializer(following, many=True)
+        return Response(serializer.data)
+
 
 class PasswordResetAPIView(APIView):
     permission_classes = []  # Allow public access
