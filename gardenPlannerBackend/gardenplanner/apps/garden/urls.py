707f8a7032929ff8e97686bc1a3eded7ae7e0739
Bahadır Kuşcan <149206458+bahadirkuscan@users.noreply.github.com>
2025-04-22 03:16:51 +0300

    Added Password Reset URLs

--- gardenPlannerBackend/gardenplanner/apps/garden/urls.py
+++ gardenPlannerBackend/gardenplanner/apps/garden/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('password-reset/', views.PasswordResetAPIView.as_view(), name='password_reset'),
+    path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ] 
