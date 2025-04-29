2dfde09198a320ae417fca0e7a7b1298c4ab43f2
Ahmet Ayberk Durak <ahmetayberkdurak@hotmail.com>
2025-04-29 15:42:22 +0300

    test: add unit tests for Comment API

--- gardenPlannerBackend/gardenplanner/apps/garden/tests.py
+++ gardenPlannerBackend/gardenplanner/apps/garden/tests.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 from django.contrib.auth.models import User
-from .models import ForumPost
+from .models import ForumPost, Comment
 
 class ForumPostTests(TestCase):
 
@@ -64,3 +64,46 @@ class ForumPostTests(TestCase):
         
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(ForumPost.objects.count(), 0)
+        
+class CommentTests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', password='password')
+        self.client.force_authenticate(user=self.user)
+        self.post = ForumPost.objects.create(title='Test Post', content='Some content', author=self.user)
+
+    def test_create_comment(self):
+        url = reverse('garden:comment-list-create')
+        data = {'forum_post': self.post.id, 'content': 'This is a test comment.'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_list_comments(self):
+        Comment.objects.create(forum_post=self.post, content='Comment 1', author=self.user)
+        Comment.objects.create(forum_post=self.post, content='Comment 2', author=self.user)
+        
+        url = reverse('garden:comment-list-create')
+        response = self.client.get(url, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_update_comment(self):
+        comment = Comment.objects.create(forum_post=self.post, content='Old content', author=self.user)
+        
+        url = reverse('garden:comment-detail', kwargs={'pk': comment.pk})
+        data = {'content': 'Updated content'}
+        response = self.client.put(url, data, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        comment.refresh_from_db()
+        self.assertEqual(comment.content, 'Updated content')
+
+    def test_delete_comment(self):
+        comment = Comment.objects.create(forum_post=self.post, content='To delete', author=self.user)
+        
+        url = reverse('garden:comment-detail', kwargs={'pk': comment.pk})
+        response = self.client.delete(url, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
