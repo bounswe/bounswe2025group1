46e5b3d2b684edc842226db5a8a27f33be0f5a68
bahadirkuscan <bahadirkuscan@hotmail.com>
2025-12-10 10:23:43 +0300

    Fixed Migration

--- gardenPlannerBackend/gardenplanner/apps/garden/migrations/0017_forumpostlike_commentlike.py
+++ gardenPlannerBackend/gardenplanner/apps/garden/migrations/0017_forumpostlike_commentlike.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ("garden", "0015_notification_link"),
+        ("garden", "0016_multi_assignee_tasks"),
     ]
 
     operations = [
