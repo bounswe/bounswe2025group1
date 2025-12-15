2fb5b38cc1fd45e6876e32203b6bf1285d7dd445
bahadirkuscan <bahadirkuscan@hotmail.com>
2025-12-15 21:30:15 +0300

    Migration Fix

--- gardenPlannerBackend/gardenplanner/apps/garden/migrations/0023_add_task_timestamps.py
+++ gardenPlannerBackend/gardenplanner/apps/garden/migrations/0023_add_task_timestamps.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('garden', '0021_add_recurring_tasks'),
+        ('garden', '0022_loginotp_devicefingerprint'),
     ]
 
     operations = [
